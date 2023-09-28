361a4315b4d654a8514d1ef8c43313adb53c5859
mmarchal <marchal.maxime@live.fr>
2023-09-28 17:04:36 +0200

    fix(card): Use Stack for the selected icon and update size

--- flutter/example/lib/location_selection_card_usecase.dart
+++ flutter/example/lib/location_selection_card_usecase.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 import 'package:yaki_ui/yaki_ui.dart';
 
@@ -8,15 +9,28 @@ import 'package:yaki_ui/yaki_ui.dart';
 )
 Widget simpleLocationSelectionCard(BuildContext context) {
   return Center(
-    child: LocationSelectionCard(
-      picture: Image.network(
-        'https://picsum.photos/200',
+    child: SizedBox(
+      width: 200,
+      child: LocationSelectionCard(
+        picture: Image.asset(
+          'assets/time.png',
+          width: 110,
+          height: 110,
+        ),
+        title: context.knobs.string(
+          label: 'Card title',
+          description: 'The title of the card',
+          initialValue: 'Yaki',
+        ),
+        subtitle: context.knobs.string(
+          label: 'Card subtitle',
+          description: 'The subtitle of the card',
+          initialValue: 'Yaki subtitle',
+        ),
+        onSelectionChanged: (selected) {
+          debugPrint('Location selection changed: $selected');
+        },
       ),
-      title: 'Yaki',
-      subtitle: 'Yaki is a design system',
-      onSelectionChanged: (selected) {
-        debugPrint('Location selection changed: $selected');
-      },
     ),
   );
 }
